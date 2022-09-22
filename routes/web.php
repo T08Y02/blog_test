111da78c9abaab5c2f660602abfdb17c6a379aa7
T08Y02 <pon9q9@gmail.com>
2022-09-22 16:59:19 +0900

    ブログ削除機能の実装

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class ,'show']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class,'delete']);
 
 /*Route::get('/', function () {
     return view('welcome');
