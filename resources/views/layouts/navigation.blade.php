ddd1a09cc9c2d83602a2b4600064819fcb2cf9fb
T08Y02 <pon9q9@gmail.com>
2023-02-23 21:25:01 +0900

    test

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,7 +16,7 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                     <x-nav-link :href="route('index')" :active="request()->routeIs('index')">
-                        {{ __('Index') }}
+                        {{ __('index') }}
                     </x-nav-link>
                 </div>
             </div>
